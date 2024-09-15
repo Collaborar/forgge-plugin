bcef9d822674cfe211e445045adf86f24a68b047
Paulo Ferreira <hi@paulofr.dev>
2024-09-15 12:34:10 -0300

    docs: added comments to images and spritesvg files.

---
+++ resources/scripts/frontend/images.js
@@ -0,0 +1,7 @@
+/**
+ * Images
+ * Import your images here to make it available from PHP
+ * \MyApp::core()->assets()>getAssetUrl('images/my-image.png')
+ */
+// eslint-ignore-next-line
+// import '@images/my-image.png';
